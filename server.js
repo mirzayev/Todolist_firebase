eeb10c57f76a8439c5c8cf6cd53b98e699bdb398
mirzayev <mirzayevfarid@gmail.com>
2018-06-10 20:52:41 +0400

    server setup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const router = express.Router();
 
 // set port
 const port = process.env.PORT || 8080;
@@ -8,34 +8,9 @@ app.use(express.static(__dirname + "/public"));
 
 //routes
 app.get("/", function(req, res) {
-  res.render("/public/register/register");
-});
-
-app.get("/sample", function(req, res) {
-  res.send("this is sample");
+  res.render("hello world");
 });
 
 app.listen(port, function() {
   console.log("listening port " + port);
 });
-
-router.use(function(req, res, next) {
-  // log each request to the console
-  console.log(req.method, req.url);
-
-  // continue doing what we were doing and go to the route
-  next();
-});
-
-// home page route (http://localhost:8080)
-router.get("/", function(req, res) {
-  res.send("im the home page!");
-});
-
-// about page route (http://localhost:8080/about)
-router.get("/about", function(req, res) {
-  res.send("im the about page!");
-});
-
-// apply the routes to our application
-app.use("/", router);
